782f6e561939237c3de871273402b98b1452f98f
jeffersonximeness <jeffersonximenes1@gmail.com>
2022-03-07 13:36:58 -0300

    rotas da aplicação

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router()
+const UserController = require('../controllers/UserController')
+const AddressController = require('../controllers/AddressController')
 
 router.get('/', (req, res) => {
     res.json({ hello: 'World' })
 })
+
+router.get('/users', UserController.index)
+router.post('/users', UserController.store)
+
+router.get('/users/:user_id/addresses', AddressController.index)
+router.post('/users/:user_id/addresses', AddressController.store)
 
 module.exports = router
